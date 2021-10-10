26d3c32a8e0a33d16ddbc389e1dd8b06c9b3cb2f
BrotskyS <sergiypik.work@gmail.com>
2021-10-10 21:55:40 +0300

    done hello world app!

--- Lab1/ContentView.swift
+++ Lab1/ContentView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        print("Hello, world!")
+        return Text("Hello, world!")
             .padding()
     }
 }
